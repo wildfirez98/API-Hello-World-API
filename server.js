623516586c2708850ba84574733584d3d747cb60
Jamie <james@areafamily.us>
2022-03-14 20:19:20 -0500

    Activity complete

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
 
+// Languages Controller that is used by our Router
+const languagesController = require('./controllers/languages_controller.js')
+app.use('/languages', languagesController)
+
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to the Hello World! API')
